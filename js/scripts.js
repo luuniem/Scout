71ab7e78f890c739456f3c8bfb36e69c496bfc87
Niem Nguyen <luuniem87@gmail.com>
2016-05-19 16:42:17 -0700

    finished friends page

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(function(){
+  $("#unf1").click(function(){
+    $("#un1, #unf1").fadeOut();
+  });
+  $("#unf2").click(function(){
+    $("#un2, #unf2").fadeOut();
+  });
+  $("#unf3").click(function(){
+    $("#un3, #unf3").fadeOut();
+  });
+  $("#unf4").click(function(){
+    $("#un4, #unf4").fadeOut();
+  });
+  $("#unf5").click(function(){
+    $("#un5, #unf5").fadeOut();
+  });
+  $("#unf6").click(function(){
+    $("#un6, #unf6").fadeOut();
+  });
+  $("#unf7").click(function(){
+    $("#un7, #unf7").fadeOut();
+  });
+  $("#unf8").click(function(){
+    $("#un8, #unf8").fadeOut();
+  });
+  $("#unf9").click(function(){
+    $("#un9, #unf9").fadeOut();
+  });
+  $("#unf10").click(function(){
+    $("#un10, #unf10").fadeOut();
+  });
+
+});
